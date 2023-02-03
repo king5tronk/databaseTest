956dacacf0864664a055ab95246bc43b98197cfb
Tompa123 <mrguy_93@hotmail.com>
2023-02-03 17:31:09 +0100

    c1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException, IOException {
+
+        Properties p = new Properties();
+        p.load(new FileInputStream("/Users/tom/Desktop/Intellij Projects/databaseTest/src/Settings.properties.txt"));
+
+        try (Connection con = DriverManager.getConnection(
+                p.getProperty("connectionString"),
+                p.getProperty("name"),
+                p.getProperty("password"));
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery("select id, märke from Skor")) {
     }
+}
 }
